b56f53e73a700b4f0afa667b02b5a6df8b93d604
Kirill Tronin <troninkirill@gmail.com>
2024-02-17 15:24:59 +0700

    gov voting

--- packages/shared/lib/src/rpc_client.js
+++ packages/shared/lib/src/rpc_client.js
@@ -9,3 +9,17 @@ export async function wasmFetch(url, method, body) {
   });
   return res;
 }
+
+export async function fetchProposals(epoch) {
+  console.log("epoch", epoch);
+  const res = await fetch("https://it.api.namada.red/api/v1/chain/governance/proposals", {
+    method: "GET"
+  });
+
+  const data = await res.json();
+
+  console.log(data);
+  let resulst = data.proposals.filter(x => x.end_epoch > epoch && x.start_epoch < epoch);
+  console.log("result", resulst);
+  return JSON.stringify(resulst.map(x => x.id));
+}
